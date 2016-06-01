5c496d3ece770cbb23c8bf3578a24c2a05b274fc
Rinzler838 <dvil4688@740s-pl-193.local>
2016-06-01 08:42:33 -0600

    Coded input functions

--- src/stacks/controller/StackController.java
+++ src/stacks/controller/StackController.java
@@ -1,6 +1,71 @@
 package stacks.controller;
 
+import java.awt.Color;
+import java.util.Scanner;
+import java.util.Stack;
+
+import stacks.model.Dish;
+
 public class StackController
 {
+	private Stack<Dish> dishStack;
+	private Scanner stackScanner;
 	
+	public StackController()
+	{
+		dishStack = new Stack<Dish>();
+		stackScanner = new Scanner(System.in);
+	}
+	
+	public void start()
+	{
+		System.out.println("What would you like to do? Type \"push\", \"pop\", or \"peek\"...");
+		if (stackScanner.nextLine() == "push")
+		{
+			System.out.println("Is your plate dirty? Type \"true\" or \"false\"...");
+			boolean isDirty = stackScanner.nextBoolean();
+			
+			System.out.println("What color is your plate?");
+			String userColor = (stackScanner.nextLine());
+			Color plateColor = null;
+			try
+			{
+				plateColor = (Color) Color.class.getField(userColor).get(null);
+			}
+			catch (IllegalArgumentException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (IllegalAccessException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (NoSuchFieldException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (SecurityException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("What is the radius of your plate? Type an integer...");
+			int radius = stackScanner.nextInt();
+			
+			Dish newDish = new Dish(isDirty, plateColor, radius);
+			dishStack.push(newDish);
+		}
+		else if (stackScanner.nextLine() == "pop")
+		{
+			dishStack.pop();
+		}	
+		else if (stackScanner.nextLine() == "peek")
+		{
+			System.out.println(dishStack.peek());
+		}
+	}
 }
--- src/stacks/model/Dish.java
+++ src/stacks/model/Dish.java
@@ -7,39 +7,42 @@ public class Dish
 	private boolean isDirty;
 	private Color plateColor;
 	private int radius;
+	
+	public Dish(boolean isDirty, Color plateColor, int radius)
+	{
+		this.isDirty = isDirty;
+		this.plateColor = plateColor;
+		this.radius = radius;
+	}
 
-public void setDirty(boolean isDirty)
-{
-	this.isDirty = isDirty;
+	public void setDirty(boolean isDirty)
+	{
+		this.isDirty = isDirty;
+	}
+
+	public boolean getDirty()
+	{
+		return isDirty;
+	}
+
+	public void setColor(Color plateColor)
+	{
+		this.plateColor = plateColor;
+	}
+
+	public Color getColor()
+	{
+		return plateColor;
+	}
+
+	public void setRadius(int radius)
+	{
+		this.radius = radius;
+	}
+
+	public int getRadius()
+	{
+		return radius;
+	}
+
 }
-
-public boolean isDirty()
-{
-	return isDirty;
-}
-
-public void setColor(Color plateColor)
-{
-	this.plateColor = plateColor;
-}
-
-public Color getColor()
-{
-	return plateColor;
-}
-
-public void setRadius(int radius)
-{
-	this.radius = radius;
-}
-
-public int getRadius()
-{
-	return radius;
-}
-
-}
-
-
-
-
